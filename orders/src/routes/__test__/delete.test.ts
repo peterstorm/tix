d55813fdce149dd6fe07ef44282c3a64364b955c
Peter Storm <peter.storm@peterstorm.io>
2021-03-18 16:21:39 +0100

    fixed orders tests

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,57 +1,62 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
-import {Order, OrderStatus} from '../../models/order';
-import {Ticket} from '../../models/ticket';
-import {natsWrapper} from '../../nats-wrapper';
-import mongoose from 'mongoose';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
 
-const buildTicket = async () => {
+it('marks an order as cancelled', async () => {
+  // create a ticket with Ticket Model
   const ticket = Ticket.build({
     id: mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
-    price: 20
+    price: 20,
   });
   await ticket.save();
-  return ticket;
-}
-it('marks and orders as cancelled', async () => {
-  const ticketOne = await buildTicket();
 
-  const userOne = global.signin();
-
-  const response = await request(app)
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
     .post('/api/orders')
-    .set('Cookie', userOne)
-    .send({ ticketId: ticketOne.id })
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
     .expect(201);
 
+  // make a request to cancel the order
   await request(app)
-    .delete(`/api/orders/${response.body.order.id}`)
-    .set('Cookie', userOne)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
     .send()
     .expect(204);
 
-  const updatedOrder = await Order.findById(response.body.order.id)
+  // expectation to make sure the thing is cancelled
+  const updatedOrder = await Order.findById(order.id);
+
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it('it emits an order cancelled event', async () => {
-  const ticketOne = await buildTicket();
+it('emits a order cancelled event', async () => {
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
 
-  const userOne = global.signin();
-
-  const response = await request(app)
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
     .post('/api/orders')
-    .set('Cookie', userOne)
-    .send({ ticketId: ticketOne.id })
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
     .expect(201);
 
+  // make a request to cancel the order
   await request(app)
-    .delete(`/api/orders/${response.body.order.id}`)
-    .set('Cookie', userOne)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
     .send()
     .expect(204);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
-
 });
--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,33 +1,56 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
-import {Ticket} from '../../models/ticket';
-import mongoose from 'mongoose';
+import { Ticket } from '../../models/ticket';
 
-const buildTicket = async () => {
+it('fetches the order', async () => {
+  // Create a ticket
   const ticket = Ticket.build({
     id: mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
-    price: 20
+    price: 20,
   });
   await ticket.save();
-  return ticket;
-}
-it('fetches the order', async () => {
-  const ticketOne = await buildTicket();
 
-  const userOne = global.signin();
-
-  const response = await request(app)
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
     .post('/api/orders')
-    .set('Cookie', userOne)
-    .send({ ticketId: ticketOne.id })
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
     .expect(201);
 
-  const order = await request(app)
-    .get(`/api/orders/${response.body.order.id}`)
-    .set('Cookie', userOne)
+  // make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
     .send()
     .expect(200);
 
-  expect(order.body.id).toEqual(response.body.order.id);
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another users order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the order
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
 });
